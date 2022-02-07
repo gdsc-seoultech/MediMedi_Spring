b6ec01a9acd273b923040f6bc3d53a22475e518b
twinklesu <twinklesu914@gmail.com>
2022-02-07 15:30:32 +0900

    initial commit

---
+++ src/main/java/seoultech/gdsc/mediMedi/MediMediApplication.java
@@ -0,0 +1,13 @@
+package seoultech.gdsc.mediMedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediMediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediMediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seoultech/gdsc/mediMedi/MediMediApplicationTests.java
@@ -0,0 +1,13 @@
+package seoultech.gdsc.mediMedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediMediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
